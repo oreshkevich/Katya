c154da12d89cf0e33ad187d920d9fdf95e9624f3
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-02-11 21:00:31 +0300

    fix

--- js/playList.js
+++ js/playList.js
@@ -1,22 +1,23 @@
 const playList = [
-  {      
-    title: 'Aqua Caelestis',
-    src: '../momentum/assets/sounds/Aqua Caelestis.mp3',
-    duration: '00:58'
-  },  {      
-    title: 'River Flows In You',
-    src: '../momentum/assets/sounds/River Flows In You.mp3',
-    duration: '03:50'
+  {
+    title: "Aqua Caelestis",
+    src: "./assets/sounds/Aqua Caelestis.mp3",
+    duration: "00:58",
   },
-  {      
-    title: 'Summer Wind',
-    src: '../momentum/assets/sounds/Summer Wind.mp3',
-    duration: '05:05'
+  {
+    title: "River Flows In You",
+    src: "./assets/sounds/River Flows In You.mp3",
+    duration: "03:50",
   },
-  {      
-    title: 'Ennio Morricone',
-    src: '../momentum/assets/sounds/Ennio Morricone.mp3',
-    duration: '05:03'
-  }
-]
+  {
+    title: "Summer Wind",
+    src: "./assets/sounds/Summer Wind.mp3",
+    duration: "05:05",
+  },
+  {
+    title: "Ennio Morricone",
+    src: "./assets/sounds/Ennio Morricone.mp3",
+    duration: "05:03",
+  },
+];
 export default playList;
